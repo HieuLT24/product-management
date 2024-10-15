84367bd15c6c8cc430991a100ccc65557f8c5d35
HieuLT24 <duchieuphung5@gmail.com>
2024-10-15 17:20:43 +0700

    cai dat database

--- product-management/index.js
+++ product-management/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 require("dotenv").config();
+const database = require("./config/database")
 
 const route = require("./routes/client/index.route.js")
+
+database.connect();
 
 const app = express();
 const port = process.env.PORT;
